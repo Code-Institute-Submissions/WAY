008fba15f5727f66531a1b43e3b01709dfe1e468
ByIlsa <byilsa@aukjevanderwal.nl>
2020-08-17 07:29:36 +0000

    Rearranged shared json file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-/***HIDE QUESTION SECTION WHEN THE PAGE IS LOADED***/
+/***Check if DOM is ready for JavaScript***/
 $(document).ready(function () {
-  $(".question").hide();
-});
+  
 
 
 function startGame() {
-  /***ON CLICK Start Quiz BUTTON HIDE LANDING PAGE AND SHOW QUESTION IN QUIZ***/
+  /***ON CLICK Enter at own risk BUTTON HIDE LANDING PAGE AND SHOW QUESTION IN QUIZ***/
   $("#start-btn").click(function () {
     $("#landing-page").hide("slow");
     $(".question").show("slow");
   });
 }
+});
