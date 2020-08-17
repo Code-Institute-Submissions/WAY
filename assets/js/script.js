3f6e84ceec9f0b9e3e56b9fe07fe4233cc9507fe
ByIlsa <byilsa@aukjevanderwal.nl>
2020-08-17 11:06:39 +0000

    Attempt to get hide/show working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,18 @@ const question = document.getElementById('question');
 const quizProgress = document.getElementById('quizProgress');
 const progressBarFull = document.getElementById('progressBarFull');
 
-/***Check if DOM is ready for JavaScript***/
 $(document).ready(function () {
-    $("#start-btn").on("click", function() {
-  
-function startGame() {
-  /***ON CLICK Enter at own risk BUTTON HIDE LANDING PAGE AND SHOW QUESTION IN QUIZ***/
-  $("#start-btn").click(function () {
-    $("#landing-page").hide("slow");
+    $("#landing-page").click(function() {
+         $("#landing-page").hide("slow");
     $(".question").show("slow");
   });
-}
+})
+
 
 fetch("../js/shared.json")
 .then(res => res.json())
 .then(data => console.log(data))
-})
+
    .then((loadedQuestions) => {
         questions = loadedQuestions;
         startGame();
@@ -26,4 +22,3 @@ fetch("../js/shared.json")
         console.error(err);
     });
 
-})
