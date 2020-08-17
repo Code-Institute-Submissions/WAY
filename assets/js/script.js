48c272a5809507a57aade31211c39b3eec5e43e7
ByIlsa <byilsa@aukjevanderwal.nl>
2020-08-17 08:58:50 +0000

    Add two const I might need...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+const question = document.getElementById('question');
+const quizProgress = document.getElementById('quizProgress');
+
 /***Check if DOM is ready for JavaScript***/
 $(document).ready(function () {
     $("#start-btn").on("click", function() {
@@ -9,6 +12,7 @@ function startGame() {
     $(".question").show("slow");
   });
 }
+
 fetch("../js/shared.json")
 .then(res => res.json())
 .then(data => console.log(data))
