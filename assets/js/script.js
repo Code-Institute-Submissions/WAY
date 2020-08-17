dd9ad03f4eea257beecd5b7dac319bba23a72e89
ByIlsa <byilsa@aukjevanderwal.nl>
2020-08-17 08:51:21 +0000

    Add fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@
 $(document).ready(function () {
     $("#start-btn").on("click", function() {
   
-
-
 function startGame() {
   /***ON CLICK Enter at own risk BUTTON HIDE LANDING PAGE AND SHOW QUESTION IN QUIZ***/
   $("#start-btn").click(function () {
