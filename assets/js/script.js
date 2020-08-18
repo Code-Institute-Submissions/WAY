a0ac9543c9f83643cb3931d88af29bda8acdb427
ByIlsa <byilsa@aukjevanderwal.nl>
2020-08-18 10:49:05 +0000

    Add some meta data to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 /*const question = document.getElementById('question');
 const quizProgress = document.getElementById('quizProgress');
-const progressBarFull = document.getElementById('progressBarFull');
 
-$(document).ready(function () {
-  fetchData("game.json");
-});
 */
+$(document).ready(function () {
+  fetchData("shared.json");
+});
+
 $(document).ready(function () {
    $("#question").hide();
   $("#start-btn").click(function(){
