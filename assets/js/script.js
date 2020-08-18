5005d9cd7fb7d40cad2da922bab8c8983fd3a003
ByIlsa <byilsa@aukjevanderwal.nl>
2020-08-18 10:28:54 +0000

    Make question section hide on load, then showr on click and make landing-page hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,16 @@
 /*const question = document.getElementById('question');
 const quizProgress = document.getElementById('quizProgress');
-const progressBarFull = document.getElementById('progressBarFull');*/
+const progressBarFull = document.getElementById('progressBarFull');
 
 $(document).ready(function () {
-    $("#start-btn").click(function() {
-         $("#landing-page").hide("slow");
-    $("#question").show("slow");
-  });
-})
-
-async function loadJSON (url) {
-  const res = await fetch(url);
-  return await res.json();
-}
-
-loadJSON('shared.json').then(data => {
-  console.log(data[0].name);
+  fetchData("game.json");
 });
-
-
+*/
+$(document).ready(function () {
+   $("#question").hide();
+  $("#start-btn").click(function(){
+    $("#landing-page").hide();
+    $("#question").show();
+  });
+  
+});
