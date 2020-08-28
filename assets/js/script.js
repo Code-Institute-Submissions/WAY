f07f85e89f9e78f30647b7074bb96544269bb979
ByIlsa <byilsa@aukjevanderwal.nl>
2020-08-28 11:51:25 +0000

    Clean up for more clarity

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,15 +35,13 @@ $(document).ready(function () {
 //store outcome in variable
 //for id 2, store outcome in variabel depending on choice (healer, dps, tank)
 //for id 3-11, if yes, store in variabel and go to alli or horde, depending on choice made in id1 and id2
-//if nom continue to next question
+//if no continue to next question
 
 function buildGame (quiz, questionNumber) {
    const start = quiz.find(game => game.id === questionNumber)
           let i;
     for (i = 0; i < start.question.length;i++);
     
-    //button needs to go to right next question
-    //if it comes to end of shared, insert into a result file for later use
     questionRef.innerHTML = start.question
     //loop through anwers array
     //append text to answer buttons
