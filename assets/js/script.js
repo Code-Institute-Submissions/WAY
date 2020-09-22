d26fc1492fe5fd02811559e826c9fd6fcdf72a40
ByIlsa <byilsa@aukjevanderwal.nl>
2020-09-22 10:15:12 +0000

    Cleanup script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //jshint esversion: 6
-/*Some code structure ideas relating to how to display the next question and answer were referenced from: https://github.com/WebDevSimplified/JavaScript-Text-Adventure */
+//Some code structure ideas relating to how to display the next question and answer were referenced from: https://github.com/WebDevSimplified/JavaScript-Text-Adventure
 
 const questionRef = document.querySelector('#question-info');
 const answerButtonRef = document.querySelectorAll('.answer-button');
@@ -29,7 +29,6 @@ const fetchData = (jsonFile) => {
     return fetch(`assets/data/${jsonFile}`)
         .then((res) => res.json())
         .then((gameFile) => {
-            //console.log(gameFile);
             quiz = gameFile;
             showQuestion(1);
         })
@@ -60,23 +59,20 @@ const showQuestion = (questionIndex) => {
         };
     }
     
-    /* When an image is available, this will make it visible in the DOM */
+// When an image is available, this will make it visible in the DOM 
     const answers = questions.answers;
     for (let i = 0; i < imageRef.length; i++) {
-        //   console.log(questions.answers.source);
         if (i < answers.length) {
             if (answers[i].source != null){
-                // console.log(images);
                 imageRef[i].classList.remove("hide");
                 imageRef[i].innerHTML = '<img src="' + answers[i].source + '" class="image">';
             }
             else {
-                // This is for when answers don't have sources
+// This is for when answers don't have sources
                 imageRef[i].classList.add("hide");
-            }
-
-        } else {
-            // This is for when there are fewer answers than imageRefs
+            }  
+        }   else {
+// This is for when there are fewer answers than imageRefs
             imageRef[i].classList.add("hide");
         }
     }
@@ -86,15 +82,11 @@ const showQuestion = (questionIndex) => {
  * Function to get
  * the next question by id
  */
-
 const selectAnswer = (id) => {
-    //  console.log(selectAnswer, id);
     nextQuestion = parseInt(id, 10);
     if (nextQuestion <= 0) {
-        //  console.log(showQuestion);
         showQuestion(1);
     } else {
-        //  console.log(showQuestion. nextQuestion)
         showQuestion(nextQuestion);
     }
 }
