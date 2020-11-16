4caf4a0b1e2ae0870a3d82af0451ed9952988a04
ByIlsa <byilsa@aukjevanderwal.nl>
2020-11-16 16:11:07 +0000

    Finish showResults function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,148 +1,145 @@
 //jshint esversion: 6
 //$(document).ready(function() {
-const questionRef = document.querySelector('#questions');
-const answerButtonRef = document.querySelectorAll('.answer-button');
-const imageRef = document.querySelectorAll('.image');
-
-const resultRef = document.getElementById('#result');
-const factionRef = document.getElementById('factions');
-const roleRef = document.getElementById('roles');
-const classRef = document.getElementById('classes');
-const raceRef = document.getElementById('races');
-
+const questionRef = document.querySelector("#questions");
+const answerButtonRef = document.querySelectorAll(".answer-button");
+const imageRef = document.querySelectorAll(".image");
+const resultImageRef = document.querySelector(".result-image");
+const resultRef = document.getElementById("result");
+const factionRef = document.getElementById("factions");
+const roleRef = document.getElementById("roles");
+const classRef = document.getElementById("classes");
+const raceRef = document.getElementById("races");
 
 let quiz;
 
+$(document).ready(function () {
+  $("#WAY").fadeIn("slow");
+  $("#game-container").toggle();
 
-$(document).ready(function() {
-    $("#WAY").fadeIn("slow");
-    $("#game-container").toggle();
-    
-    $("#start-btn").click(function() {
-        $("#landing-page, #game-container").toggle();
+  $("#start-btn").click(function () {
+    $("#landing-page, #game-container").toggle();
+  });
+  for (let i = 0; i < answerButtonRef.length; i++) {
+    answerButtonRef[i].addEventListener("click", function (e) {
+      selectAnswer(e.target.dataset.nextQuestion);
     });
-    for (let i = 0; i < answerButtonRef.length; i++) {
-        answerButtonRef[i].addEventListener("click", function(e) {
-            selectAnswer(e.target.dataset.nextQuestion);
-
-        });
-    }
-    fetchData("gamedata.json"); 
+  }
+  fetchData("gamedata.json");
 });
 
 /**
- * Fetch for shared 
- * questions at start 
- * game 
+ * Fetch for shared
+ * questions at start
+ * game
  */
 
 const fetchData = (jsonFile) => {
-    return fetch(`assets/data/${jsonFile}`)
-        .then((res) => res.json())
-        .then((gameFile) => {
-            quiz = gameFile;
-            showQuestion(1);
-        })
-        .catch((err) => console.log(err));
+  return fetch(`assets/data/${jsonFile}`)
+    .then((res) => res.json())
+    .then((gameFile) => {
+      quiz = gameFile;
+      showQuestion(1);
+    })
+    .catch((err) => console.log(err));
 };
-
-
 
 /**
  * Function to get the
  * first question and answers
- * and inject them into their 
+ * and inject them into their
  * HTML element
  */
 
 const showQuestion = (questionIndex) => {
-    const questions = quiz.find(questions => questions.id === questionIndex);
-    const answerStrings = questions.answers;
-    questionRef.innerHTML = questions.question;
-    for (let i = 0; i < answerButtonRef.length; i++) {
-        if (questions.answers) {
-            if (i < answerStrings.length) {
-                answerButtonRef[i].classList.remove("hide");
-                answerButtonRef[i].innerText = answerStrings[i].text;
-                answerButtonRef[i].dataset.nextQuestion = answerStrings[i].nextQuestionId;
-            } else {
-                answerButtonRef[i].classList.add("hide");
-            }
-        }
-    }  
-    showImages(questions);
-};  
-// When an image is available, this will make it visible in the DOM
-const showImages = (questions) => { 
-    const answers = questions.answers;
-    for (let i = 0; i < imageRef.length; i++) {
-        if (i < answers.length) {
-            if (answers[i].source != null){
-                imageRef[i].classList.remove("hide");
-                imageRef[i].innerHTML = '<img src="' + answers[i].source + '" class="image">';
-            }	            
-            else {	
-// This is for when answers don't have sources
-                imageRef[i].classList.add("hide");
-            }  	            
-        }   else {	
-// This is for when there are fewer answers than imageRefs
-                imageRef[i].classList.add("hide");
+  const questions = quiz.find((questions) => questions.id === questionIndex);
+  const answerStrings = questions.answers;
+  questionRef.innerHTML = questions.question;
+  for (let i = 0; i < answerButtonRef.length; i++) {
+    if (questions.answers) {
+      if (i < answerStrings.length) {
+        answerButtonRef[i].classList.remove("hide");
+        answerButtonRef[i].innerText = answerStrings[i].text;
+        answerButtonRef[i].dataset.nextQuestion =
+          answerStrings[i].nextQuestionId;
+      } else {
+        answerButtonRef[i].classList.add("hide");
+      }
     }
-    }	
-displayResult(questions);
-};  
-/** 
- * Displaying the 
+  }
+  showImages(questions);
+};
+// When an image is available, this will make it visible in the DOM
+const showImages = (questions) => {
+  const answers = questions.answers;
+  for (let i = 0; i < imageRef.length; i++) {
+    if (i < answers?.length) {
+      if (answers[i].source != null) {
+        imageRef[i].classList.remove("hide");
+        imageRef[i].innerHTML =
+          '<img src="' + answers[i].source + '" class="image">';
+      } else {
+        // This is for when answers don't have sources
+        imageRef[i].classList.add("hide");
+      }
+    } else {
+        if (answers === undefined) {
+        displayResult(questions);
+        imageRef[i].classList.add("hide");
+        for (let j = 0; j < answerButtonRef.length; j++) {
+            answerButtonRef[j].classList.add("hide");
+        }
+      } else {
+        // This is for when answers don't have sources
+        imageRef[i].classList.add("hide");
+      }
+    }
+  }
+};
+/**
+ * Displaying the
  * results
-*/
+ */
 
 //check against undefined, when undefined, empty div "" empty string? (hide), when not undefined, display div result
 // hide/show result div, fill the faction/role etc div id's with results
 
 const displayResult = (questions) => {
-    console.log(resultRef);
-    if (resultRef.classList.includes("hide")) {
+  if (questions.question === "The results are in!") {
+    factionRef.innerHTML = "Your faction is: " + questions.factions;
 
-        console.log(resultRef);
-    //hide result div
-    resultRef.style.display="none";
-}
+    roleRef.innerHTML = "Your role is: " + questions.roles;
 
-else {
-    // this needs to go in there respective result div:
-    factionRef.innerHTML = 'Your faction is: ' + questions.factions;
+    classRef.innerHTML = "Your class is: " + questions.classes;
 
-    roleRef.innerHTML = 'Your role is: ' + questions.roles;
+    raceRef.innerHTML = "Your race is: " + questions.races;
 
-    classRef.innerHTML = 'Your class is: ' + questions.classes;
-
-    raceRef.innerHTML = 'Your race is: ' + questions.races;
+    resultImageRef.innerHTML = '<img src="' + questions.source + '" class="result-image">';
+    console.log(resultImageRef);
 
     //show result div
-    resultRef.style.display="block";
+    resultRef.classList.remove("hide");
     //hide questions div
-    questionRef.style.display="none";
-}
+    questionRef.classList.add("hide");
+  } else {
+    resultRef.classList.add("hide");
+  }
 };
 
 //Add an image to the result, might need the class to change, maybe results-image class?
 
 //imageRef.innerHTML = '<img src="' + questions.source + '" class="result-image">';
 
-
-/** 
+/**
  * Function to get
  * the next question
  * by id
  */
 
 const selectAnswer = (id) => {
-    nextQuestion = parseInt(id, 10);
-    if (nextQuestion <= 0) {
-        showQuestion(1);
-    } else {
-        showQuestion(nextQuestion);
-    }
-};	
-
+  nextQuestion = parseInt(id, 10);
+  if (nextQuestion <= 0) {
+    showQuestion(1);
+  } else {
+    showQuestion(nextQuestion);
+  }
+};
